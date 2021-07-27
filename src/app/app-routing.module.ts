52d176fe7ad818e4f6b10578b436c4997fbf3ae5
shaun <shaun.sun@towify.com>
2021-07-27 09:37:15 +0800

    gallery demo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {GalleryComponent} from "./gallery/gallery/gallery.component";
+
+const routes: Routes = [
+  {path: 'gallery', component: GalleryComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/unsplash.photo.ts
@@ -0,0 +1,15 @@
+export type UnsplashPhoto = {
+  id: string;
+  width: number;
+  height: number;
+  urls: UnsplashUrls;
+  displayWidth?: number;
+}
+
+export type UnsplashUrls = {
+  raw: string;
+  full: string;
+  regular: string;
+  small: string;
+  thumb: string;
+}
